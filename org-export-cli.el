626ea612cd4607d7d0b95fd0f758016fe668549c
Noah Hoffman <noah.hoffman@gmail.com>
2017-01-02 22:38:44 -0800

    allow emacs 24

--- org-export-cli.el
+++ org-export-cli.el
@@ -1,5 +1,5 @@
-(unless (>= emacs-major-version 25)
-  (error "Error: emacs version 25 or greater is required"))
+(unless (>= emacs-major-version 24)
+  (error "Error: emacs version 24 or greater is required"))
 
 (setq lexical-binding t)
 (provide 'cli)
