a8acf0977f64b88c30836d967cb104a22d055fa5
Noah Hoffman <noah.hoffman@gmail.com>
2017-01-04 09:28:53 -0800

    hopefully fix test for emacs 24

--- tests/test.sh
+++ tests/test.sh
@@ -4,7 +4,7 @@ set -e
 
 ./org-export html --infile README.org
 
-find ~/.org-export -name 'ob-*.el'
+find ~/.org-export -name 'ob-*.el' | sort
 
 ./org-export html --infile tests/test_python.org
 
@@ -17,7 +17,7 @@ grep -q YYYYY tests/test_python.html
 ./org-export html --infile tests/test_shell.org
 
 # code block producing XXXXX should not have been evaluated...
-test -z $(grep '1 2 3 4 5' tests/test_shell.html)
+test -z $(grep XXXXX tests/test_shell.html)
 
 # but code block producing YYYYY should have been
-grep -q '6 7 8 9 10' tests/test_shell.html
+grep -q YYYYY tests/test_shell.html
