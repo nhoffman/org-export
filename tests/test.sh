d8a687f1b1a451c9ec40b7d7f15116507e3b0b67
Noah Hoffman <noah.hoffman@gmail.com>
2017-01-03 21:31:37 -0800

    test.sh should exit on failure

--- tests/test.sh
+++ tests/test.sh
@@ -2,19 +2,21 @@
 
 set -e
 
+ls ~/.org-export/el-get/org/ob-sh*.el
+
 ./org-export html --infile README.org
 ./org-export html --infile tests/test_python.org
 
 # code block producing XXXXX should not have been evaluated...
-test -z $(grep XXXXX tests/test_python.html) || echo fail
+test -z $(grep XXXXX tests/test_python.html)
 
 # but code block producing YYYYY should have been
-grep -q YYYYY tests/test_python.html || echo fail
+grep -q YYYYY tests/test_python.html
 
 ./org-export html --infile tests/test_shell.org
 
 # code block producing XXXXX should not have been evaluated...
-test -z $(grep '1 2 3 4 5' tests/test_shell.html) || echo fail
+test -z $(grep '1 2 3 4 5' tests/test_shell.html)
 
 # but code block producing YYYYY should have been
-grep -q '6 7 8 9 10' tests/test_shell.html || echo fail
+grep -q '6 7 8 9 10' tests/test_shell.html
