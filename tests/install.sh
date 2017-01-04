954ba3769ec4d1a334837f3367f4ab58ff93a4a0
Noah Hoffman <noah.hoffman@gmail.com>
2017-01-04 07:51:30 -0800

    os-specific install

--- tests/install.sh
+++ tests/install.sh
@@ -1,7 +1,23 @@
-#!/bin/bash -x
+#!/bin/bash
 
 set -e
 
-brew update
-brew install emacs
-brew linkapps emacs
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    brew update
+    brew install emacs
+    brew linkapps emacs
+elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    if [[ "$EMACS" == "emacs24" ]]; then
+        sudo add-apt-repository -y ppa:cassou/emacs
+        sudo apt-get -qq update
+        sudo apt-get -qq -f install
+        sudo apt-get -qq install emacs24
+    elif [[ "$EMACS" == "emacs25" ]]; then
+        sudo add-apt-repository -y ppa:kelleyk/emacs
+        sudo apt-get -qq update
+        sudo apt-get -qq -f install
+        sudo apt-get -qq install emacs25
+    fi
+else
+    echo "unrecognized value of TRAVIS_OS_NAME: $TRAVIS_OS_NAME"
+fi
