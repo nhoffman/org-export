8f4e4f19f9bc0ba0d080024230557e4abe6c1932
Noah Hoffman <noah.hoffman@gmail.com>
2019-04-07 12:16:00 -0700

    attempt to fix travis

--- tests/install.sh
+++ tests/install.sh
@@ -4,21 +4,20 @@ set -e
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     brew update
-    brew install libressl
-    brew install emacs --with-gnutls
+    brew install gnutls
+    brew install emacs
     brew linkapps emacs
 elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     if [[ "$EMACS_VERSION" == "24" ]]; then
-        sudo add-apt-repository -y ppa:cassou/emacs
-        sudo apt-get -qq update
-        sudo apt-get -qq -f install
-        sudo apt-get -qq install emacs24
-    elif [[ "$EMACS_VERSION" == "25" ]]; then
-        sudo add-apt-repository -y ppa:kelleyk/emacs
-        sudo apt-get -qq update
-        sudo apt-get -qq -f install
-        sudo apt-get -qq install emacs25
+        PPA="ppa:cassou/emacs"
+    else
+	# emacs 25 and 26
+	PPA="ppa:kelleyk/emacs"
     fi
+    sudo add-apt-repository -y "$PPA"
+    sudo apt-get -qq update
+    sudo apt-get -qq -f install
+    sudo apt-get -qq install emacs$EMACS_VERSION
 else
     echo "unrecognized value of TRAVIS_OS_NAME: $TRAVIS_OS_NAME"
 fi
