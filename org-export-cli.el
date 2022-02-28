630124a61545c00eb6f395e15aaa8ce3b0c64119
Noah Hoffman <noah.hoffman@gmail.com>
2022-02-27 20:46:32 -0800

    remove TLS workaround

--- org-export-cli.el
+++ org-export-cli.el
@@ -131,12 +131,12 @@ value of `cli-do-nothing'.
 
   ;; fix TLS certificate errors
   ;; http://emacs.stackexchange.com/questions/18045/how-can-i-retrieve-an-https-url-on-mac-os-x-without-warnings-about-an-untrusted
-  (if (and (eq system-type 'darwin)
-	   (file-exists-p "/usr/local/etc/libressl/cert.pem"))
-      (progn
-	(require 'gnutls)
-	(setq gnutls-verify-error t)
-	(add-to-list 'gnutls-trustfiles "/usr/local/etc/libressl/cert.pem")))
+  ;; (if (and (eq system-type 'darwin)
+  ;;          (file-exists-p "/usr/local/etc/libressl/cert.pem"))
+  ;;     (progn
+  ;;       (require 'gnutls)
+  ;;       (setq gnutls-verify-error t)
+  ;;       (add-to-list 'gnutls-trustfiles "/usr/local/etc/libressl/cert.pem")))
 
   ;; install el-get if necessary
   (setq cli-el-get-repo
